3bf4ac922a988c8788a8dd60e1aa54372fae22ca
Vadim <vingvadim@gmail.com>
2023-12-02 22:08:34 +0300

    model deploy to streamlit with gui

---
+++ streamlit_app/app.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import pickle
+import wget
+import keras
+import tensorflow as tf
+import tensorflow_text
+import tensorflow_hub as hub
+import os
+# Download/load model and encoder function
+@st.cache_resource
+def get_encoder_and_model():
+    """Returns objects `LabelEncoder` and `keras.Model`
+
+    Returns:
+        tuple: (encoder, model)
+    """
+    folder_name='./streamlit_app/model/'
+    encoder_filename = folder_name + 'le_encoder.pkl'
+    model_filename = folder_name + 'bert_tuned.h5'
+    # Download model and encoder
+    if not os.path.exists(folder_name): 
+        os.mkdir(folder_name)
+        url_encoder = r'https://drive.google.com/u/0/uc?id=1qv5G1m1f9vPAVEYT6CauRzIdtxAWkwLi&export=download&confirm=yes'
+        url_model = r'https://drive.google.com/u/0/uc?id=16AJS38-rJ5HOfRtK9eJnHdhyf9G5UFDi&export=download&confirm=yes'
+        wget.download(url_encoder, out=encoder_filename)
+        wget.download(url_model, out=model_filename)
+    # Deserialize encoder
+    with open(encoder_filename, 'rb') as encoder_file:
+        label_encoder = pickle.load(encoder_file)
+    # Load pretrained model
+    model = keras.models.load_model(model_filename, custom_objects={'KerasLayer': hub.KerasLayer})
+    # Return encoder and model
+    return label_encoder, model
+# Load encoder and model
+label_encoder, model = get_encoder_and_model()
+# Lambda function for prediction
+prediction = lambda desc: label_encoder.inverse_transform(model.predict(desc, verbose=0).argmax(axis=1))[0]
+# Show title
+st.title('Предсказание жанра фильма по его описанию')
+# Show text input
+description = st.text_area('Введите описание фильма', '', key='desc')
+# Add clear button
+def clear_btn():
+    st.session_state.desc = ''
+st.button('Очистить', key='clear', on_click=clear_btn)
+# Show prediction
+if description:
+    st.markdown(f'#### Предсказанный жанр: **{prediction([description])}**')
